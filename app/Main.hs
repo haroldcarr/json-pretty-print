277cbc1732ee7bace80af080a7abe04f3def1a1b
Harold Carr <harold.carr@gmail.com>
2016-04-16 09:00:13 -0700

    handle wrong number of args

--- app/Main.hs
+++ app/Main.hs
@@ -2,14 +2,15 @@ module Main where
 
 import           JSONPP
 import           System.Environment
-import           Text.JSON
-import           Text.JSON.Pretty
 
 main :: IO ()
 main = do
-    [filename] <- getArgs
-    r <- pp filename
-    case r of
-        Left  e -> error e
-        Right d -> print d
-    return ()
+    as <- getArgs
+    case as of
+      [filename] -> do
+          r <- pp filename
+          case r of
+            Left  e -> error e
+            Right d -> print d
+          return ()
+      _ -> error "Usage: jpp <filename>"
